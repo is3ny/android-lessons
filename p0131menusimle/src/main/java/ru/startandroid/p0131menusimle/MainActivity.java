afb02808964ab7389b6b6407c5a3333080668f8d
is3ny <supremeep@yandex.ru>
2020-03-27 22:18:37 +0300

    Lesson 13

---
+++ p0131menusimle/src/main/java/ru/startandroid/p0131menusimle/MainActivity.java
@@ -0,0 +1,33 @@
+package ru.startandroid.p0131menusimle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Menu1");
+        menu.add("Menu2");
+        menu.add("Menu3");
+        menu.add("Menu4");
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
+    }
+}
