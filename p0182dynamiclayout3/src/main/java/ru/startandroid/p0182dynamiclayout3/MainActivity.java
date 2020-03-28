d3dc0ac2fb71d056ca8573076acd19e5cd314f9e
is3ny <supremeep@yandex.ru>
2020-03-28 15:58:53 +0300

    Lesson 18 -- Changing the properties of view directly in code. SeekView introduction.

---
+++ p0182dynamiclayout3/src/main/java/ru/startandroid/p0182dynamiclayout3/MainActivity.java
@@ -0,0 +1,54 @@
+package ru.startandroid.p0182dynamiclayout3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+    SeekBar sbWeight;
+    Button btn1;
+    Button btn2;
+
+    LinearLayout.LayoutParams lParams1;
+    LinearLayout.LayoutParams lParams2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sbWeight = (SeekBar) findViewById(R.id.sbWeight);
+        sbWeight.setOnSeekBarChangeListener(this);
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+
+        lParams1 = (LinearLayout.LayoutParams) btn1.getLayoutParams();
+        lParams2 = (LinearLayout.LayoutParams) btn2.getLayoutParams();
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        lParams1.weight = progress;
+        lParams2.weight = seekBar.getMax() - progress;
+
+        // btn*.requestLayout() -- to apply the change size, setText calls it automatically
+        btn1.setText(String.valueOf(lParams1.weight));
+        btn2.setText(String.valueOf(lParams2.weight));
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        return;
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        return;
+    }
+
+}
